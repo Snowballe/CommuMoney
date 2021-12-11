f1046042f15ad1f1efacc6fad356e1ad735f5393
Snowballe <62845660+Snowballe@users.noreply.github.com>
2021-12-11 01:36:53 +0100

    commit DAL

---
+++ CommuMoney.DAL/Depense_DepotDAL.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommuMoney.DAL
+{
+    class Depense_DepotDAL : Depot_DAL<Depenses_DAL>
+    {
+        public override void Delete(Depenses_DAL depense)
+        {
+            dbConnect();
+            commande.CommandText = "delete from Depenses where id_depense=@ID";
+            commande.Parameters.Add(new SqlParameter("@ID", depense.ID_DEPENSE));
+            var nbLigne = (int)commande.ExecuteNonQuery();
+
+            if (nbLigne!=1)
+            {
+                throw new Exception($"Impossible de supprimer le point d'ID {depense.ID_DEPENSE}");
+            }
+            
+            dbClose();
+        }
+
+      
+
+        public override List<Depenses_DAL> GetAll()
+        {
+            dbConnect();
+
+            commande.CommandText = "select id_depense, id_gens_qui_depense, argent_depense, created_at from Depenses";
+            var reader = commande.ExecuteReader();
+
+            var listeDesDepenses = new List<Depenses_DAL>();
+            while (reader.Read())
+            {
+                var dep = new Depenses_DAL(reader.GetInt32(0),
+                                            reader.GetInt32(1),
+                                            reader.GetFloat(2),
+                                            reader.GetDateTime(3));
+                listeDesDepenses.Add(dep);
+            }
+
+            dbClose();
+            return listeDesDepenses;
+
+        }
+
+
+        public override Depenses_DAL GetByID(int ID)
+        {
+            dbConnect();
+            commande.CommandText = "select id_depense, id_gens_qui_depense, argent_depense, created_at from Depenses where id_depenses=@ID";
+            commande.Parameters.Add(new SqlParameter("@ID", ID));
+            var reader = commande.ExecuteReader();
+
+            Depenses_DAL dep;
+
+            if (reader.Read())
+            {
+                dep = new Depenses_DAL(reader.GetInt32(0),
+                                        reader.GetInt32(1),
+                                        reader.GetFloat(2),
+                                        reader.GetDateTime(3));
+            }
+            else
+            {
+                throw new Exception($"Pas de dépense de disponible avec l'ID {ID}");
+            }
+
+            dbClose();
+            return dep;
+        }
+
+        
+
+        public override Depenses_DAL Insert(Depenses_DAL item)
+        {
+            
+        }
+
+
+        public override Depenses_DAL Update(Depenses_DAL item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommuMoney.DAL/Gens_DAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommuMoney.DAL
+{
+    public class Gens_DAL
+    {
+        public int ID { get; set; }
+    
+        public DateTime? m_created_at { get; set; }
+        public DateTime? m_updated_at { get; set; }
+
+        public string m_prenom { get; set; }
+
+        public Gens_DAL(int id, DateTime? created_at, DateTime? updated_at, string prenom)
+            => (ID, m_created_at, m_updated_at, m_prenom) = (id, created_at, updated_at, prenom);
+
+        public Gens_DAL(int id, DateTime? created_at, string prenom)
+            => (ID, m_created_at, m_prenom) = (id, created_at, prenom);
+
+        //public List<Point_DAL> Points { get; set; }
+
+        ////public Polygone_DAL(List<Point_DAL> desPoints)
+        ////{
+        ////    Points = desPoints;            
+        ////}
+
+        //public Polygone_DAL(IEnumerable<Point_DAL> desPoint)
+        //            => (Points) = (desPoint.ToList());
+        //public Polygone_DAL(int id, DateTime? dateCreation, DateTime? dateModification, IEnumerable<Point_DAL> desPoint)
+        //            => (ID, DateCreation, DateModification, Points) = (id, dateCreation, dateModification, desPoint.ToList());
+    }
+}
